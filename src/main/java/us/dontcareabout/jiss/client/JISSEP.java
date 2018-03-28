de5bd0f6ced7db3112620cba6a7f039a661155b5
MontyPan <monty.pan@dontcareabout.us>
2018-03-28 22:19:29 +0800

    GWT project 基礎檔案

---
+++ src/main/java/us/dontcareabout/jiss/client/JISSEP.java
@@ -0,0 +1,25 @@
+package us.dontcareabout.jiss.client;
+
+import com.google.gwt.user.client.Window;
+
+import us.dontcareabout.gwt.client.GFEP;
+
+public class JISSEP extends GFEP {
+	public JISSEP() {}
+
+	@Override
+	protected String version() { return "0.0.1"; }
+
+	@Override
+	protected String defaultLocale() { return "zh_TW"; }
+
+	@Override
+	protected void featureFail() {
+		Window.alert("這個瀏覽器我不尬意，不給用..... \\囧/");
+	}
+
+	@Override
+	protected void start() {
+		Window.setTitle("JISS~~~~");
+	}
+}
---
+++ src/main/java/us/dontcareabout/jiss/server/RpcServiceImpl.java
@@ -0,0 +1,9 @@
+package us.dontcareabout.jiss.server;
+
+import us.dontcareabout.gwt.server.GFServiceServlet;
+import us.dontcareabout.jiss.client.RpcService;
+
+public class RpcServiceImpl extends GFServiceServlet implements RpcService {
+	private static final long serialVersionUID = 1L;
+
+}
