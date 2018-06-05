c7809db7bdb0500200dd6c81d5a1da6961affd48
MontyPan <monty.pan@dontcareabout.us>
2018-06-05 23:00:00 +0800

    增加：將 project pom.xml 轉為 Model 的 method

--- src/main/java/us/dontcareabout/jiss/server/project/Maven.java
+++ src/main/java/us/dontcareabout/jiss/server/project/Maven.java
@@ -1,14 +1,31 @@
 package us.dontcareabout.jiss.server.project;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
 import com.google.common.io.ByteStreams;
+
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+
+import us.dontcareabout.java.common.Paths;
+import us.dontcareabout.jiss.shared.Project;
 
 public class Maven {
 	//用 File.separator 有點無聊，因為不是 Windows 上也沒辦法用 bat 吧 XDDD
 	private static final String MVN = System.getenv("MAVEN_HOME") + File.separator + "bin" + File.separator + "mvn.bat";
 	private static final String[] INSTALL = {MVN, "install"};
+
+	public static Model pom(Project project) {
+		try {
+			return new MavenXpp3Reader().read(
+				new FileReader(new Paths(project.getPath()).append("pom.xml").toFile())
+			);
+		} catch (Exception e) {
+			return null;
+		}
+	}
 
 	public static String install(File folder) throws IOException {
 		return run(folder, INSTALL);
