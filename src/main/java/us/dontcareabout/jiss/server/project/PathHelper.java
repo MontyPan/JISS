6027c1a64a6d1ec3ed9ceaa0a2e1d8f59260251f
MontyPan <monty.pan@dontcareabout.us>
2018-06-06 15:29:33 +0800

    增加：maven 結構下的兩個目錄取得方式

--- src/main/java/us/dontcareabout/jiss/server/project/PathHelper.java
+++ src/main/java/us/dontcareabout/jiss/server/project/PathHelper.java
@@ -3,17 +3,33 @@ package us.dontcareabout.jiss.server.project;
 import java.io.File;
 import java.io.FileNotFoundException;
 
+import com.google.common.base.Preconditions;
+
+import org.apache.maven.model.Model;
+
 import us.dontcareabout.java.common.Paths;
 import us.dontcareabout.jiss.shared.Project;
 
 public class PathHelper {
 	private static final String[] JAVA_SRC_ROOT = {"src", "main", "java"};
+	private static final String[] WEBAPP = {"src", "main", "webapp"};
 
 	public static File packageFolder(Project project, String subPackage) {
 		return javaBasePath(project)
 			.append(pkgToStr(subPackage))
 			.existFolder()
 			.toFile();
+	}
+
+	public static File webappFolder(Project project) {
+		return new Paths(project.getPath()).append(WEBAPP).toFile();
+	}
+
+	public static File targetWarFolder(Project project) {
+		Model model = Maven.pom(project);
+		Preconditions.checkNotNull(model);
+		return new Paths(project.getPath()).append("target")
+			.append(model.getArtifactId() + "-" + model.getVersion()).toFile();
 	}
 
 	//XXX 前提假設：需要 compile 的 gwt.xml 會在 project 的 root package 下
