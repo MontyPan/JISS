03ee63bf7f1d3cdecaaeac1b292de1a19184f1da
MontyPan <monty.pan@dontcareabout.us>
2018-06-06 15:28:36 +0800

    增加：複製目錄的 method

--- src/main/java/us/dontcareabout/jiss/server/Util.java
+++ src/main/java/us/dontcareabout/jiss/server/Util.java
@@ -2,10 +2,12 @@ package us.dontcareabout.jiss.server;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
+import com.google.common.io.Files;
 import com.google.gson.Gson;
 
 public class Util {
@@ -36,6 +38,25 @@ public class Util {
 	public static void delete(File file, ArrayList<File> error) {
 		if (!file.delete()) {
 			error.add(file);
+		}
+	}
+
+
+	public static void copyFolder(File from, File to, ArrayList<File> error) {
+		if (!to.exists()) { to.mkdirs(); }
+
+		for (File f : from.listFiles()) {
+			File t = new File(to, f.getName());
+
+			if (f.isFile()) {
+				try {
+					Files.copy(f, t);
+				} catch (IOException e) {
+					error.add(f);
+				}
+			} else {
+				copyFolder(f, t, error);
+			}
 		}
 	}
 }
