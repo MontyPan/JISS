bc7d6e3ffe49181a934a31b30c880e2640dee500
MontyPan <monty.pan@dontcareabout.us>
2018-06-07 15:31:59 +0800

    getProjects() server side 實作

--- src/main/java/us/dontcareabout/jiss/client/RpcService.java
+++ src/main/java/us/dontcareabout/jiss/client/RpcService.java
@@ -1,9 +1,15 @@
 package us.dontcareabout.jiss.client;
 
+import java.util.ArrayList;
+
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+import us.dontcareabout.jiss.shared.Project;
 
 @RemoteServiceRelativePath("RPC")
 public interface RpcService extends RemoteService{
 	void convertNewline(String path) throws Exception;
+
+	ArrayList<Project> getProjects();
 }
--- src/main/java/us/dontcareabout/jiss/server/RpcServiceImpl.java
+++ src/main/java/us/dontcareabout/jiss/server/RpcServiceImpl.java
@@ -1,7 +1,11 @@
 package us.dontcareabout.jiss.server;
 
+import java.util.ArrayList;
+
 import us.dontcareabout.gwt.server.GFServiceServlet;
 import us.dontcareabout.jiss.client.RpcService;
+import us.dontcareabout.jiss.server.project.ProjectCenter;
+import us.dontcareabout.jiss.shared.Project;
 
 public class RpcServiceImpl extends GFServiceServlet implements RpcService {
 	private static final long serialVersionUID = 1L;
@@ -9,5 +13,10 @@ public class RpcServiceImpl extends GFServiceServlet implements RpcService {
 	@Override
 	public void convertNewline(String path) throws Exception {
 		Converter.newline(path);
+	}
+
+	@Override
+	public ArrayList<Project> getProjects() {
+		return ProjectCenter.getProjects();
 	}
 }
